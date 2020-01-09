7eadfc119051782267df8a2ba31a8480d5ae6682
judgelord <devin.jl@gmail.com>
2020-01-09 09:47:56 -0600

    now uses 2018 DIME rdata

--- matchingDIMEwithDSG.R
+++ matchingDIMEwithDSG.R
@@ -15,13 +15,17 @@ load(here("data/dime_CGS_matches.Rdata"))
 # inspect
 dime %>% select(last_name, first_name, middle_initial, most.recent.contributor.name) %>% top_n(100) %>% kable()
 
+dime %<>% mutate_if(is.factor, as.character)
 
+dime$most.recent.contributor.zipcode %>% head() %>% str_squish() %<>% str_sub(1,5) 
+dime$most.recent.contributor.zipcode %<>% str_squish() %<>% str_sub(1,5) 
 #######################################################
 # CGS #
 #######
 d <- readxl::read_xlsx(here("data/CSG Directory.xlsx")) %>% 
   filter(!is.na(LAST_NAME)) %>%
-  mutate(id = row_number(), match = NA) %>% # add id
+  mutate(id = row_number(), 
+         match = NA) %>% # add id
   rename(WORK_CITY = CITY) %>% 
   rename(WORK_ADDRESS_1 = ADDRESS_1) %>%
   rename(WORK_ADDRESS_2 = ADDRESS_2) 
@@ -29,33 +33,37 @@ d <- readxl::read_xlsx(here("data/CSG Directory.xlsx")) %>%
 original <- d %<>% 
   mutate(last_name = tolower(LAST_NAME) ) %>%
   mutate(first_name = tolower(FIRST_NAME) )
+d <- original
 
 # d %<>% mutate(name = tolower(paste0(LAST_NAME,", ",FIRST_NAME))) 
 # d %<>% mutate(name2 = tolower(paste0(LAST_NAME,", ",FIRST_NAME, MIDDLE_NAME)))
 # CGSnames <- c(unique(d$name), unique() )
 
+# shared var names
 names(dime)[names(dime) %in% names(d)]
 
 
-# zip = most restrictive match = 61
-d %<>% mutate(most.recent.contributor.zipcode = as.integer(ZIP)) 
-d1 <- inner_join(d, dime)
+# zip = most restrictive match = 61, 130
+head(d$ZIP)
+head(dime$most.recent.contributor.zipcode)
+d %<>% mutate(most.recent.contributor.zipcode = ZIP)
+d1 <- inner_join(d, dime) %>% mutate(match_method = "fullname-zip")
 write.csv(d1 %>% select(match, LAST_NAME, FIRST_NAME, MIDDLE_NAME, most.recent.contributor.name, WORK_CITY, most.recent.contributor.city, WORK_ADDRESS_1,WORK_ADDRESS_2,most.recent.contributor.address, most.recent.contributor.occupation, most.recent.contributor.employer, bonica.cid, id), 
           "data/DIME-DSG-fullname-zip-matches.csv")
 d %<>% filter(!id %in% d1$id)
 
-# city = next most restrictive = 765, 667
+# city = next most restrictive = 765, 667, 1196, 937
 d %<>% select(-most.recent.contributor.zipcode)
 d %<>% mutate(most.recent.contributor.city = tolower(WORK_CITY))
-d2 <- inner_join(d, dime) 
+d2 <- inner_join(d, dime) %>% mutate(match_method = "fullname-city")
 write.csv(d2 %>% select(match, LAST_NAME, FIRST_NAME, MIDDLE_NAME, most.recent.contributor.name, WORK_CITY, most.recent.contributor.city, WORK_ADDRESS_1,WORK_ADDRESS_2,most.recent.contributor.address, most.recent.contributor.occupation, most.recent.contributor.employer, bonica.cid, id), 
           "data/DIME-DSG-fullname-city-matches.csv")
 d %<>% filter(!id %in% d2$id)
 
-# state = next most restrictive match = 4130, 2182
+# state = next most restrictive match = 4130, 2182, 2964
 d %<>% select(-most.recent.contributor.city)
 d %<>% mutate(most.recent.contributor.state = STATE_PROVINCE)
-d3 <- inner_join(d, dime) 
+d3 <- inner_join(d, dime) %>% mutate(match_method = "fullname-state")
 write.csv(d2 %>% select(match, LAST_NAME, FIRST_NAME, MIDDLE_NAME, most.recent.contributor.name, WORK_CITY, most.recent.contributor.city, WORK_ADDRESS_1,WORK_ADDRESS_2,most.recent.contributor.address, most.recent.contributor.occupation, most.recent.contributor.employer, bonica.cid, id), 
           "data/DIME-DSG-fullname-state-matches.csv")
 d %<>% filter(!id %in% d3$id)
@@ -68,26 +76,26 @@ d %<>% select(-first_name)
 
 
 
-# zip = most restrictive match = 61
+# zip = most restrictive match = 78
 # d %<>% select(-most.recent.contributor.state)
-d %<>% mutate(most.recent.contributor.zipcode = as.integer(ZIP)) 
-d4 <- inner_join(d, dime) 
+d %<>% mutate(most.recent.contributor.zipcode = ZIP)
+d4 <- inner_join(d, dime) %>% mutate(match_method = "first-initial-zip")
 write.csv(d4 %>% select(match, LAST_NAME, FIRST_NAME, MIDDLE_NAME, most.recent.contributor.name, WORK_CITY, most.recent.contributor.city, WORK_ADDRESS_1,WORK_ADDRESS_2,most.recent.contributor.address, most.recent.contributor.occupation, most.recent.contributor.employer, bonica.cid, id), 
           "data/DIME-DSG-first-initial-zip-matches.csv")
 d %<>% filter(!id %in% d4$id)
 
-# city = next most restrictive = 765, 667
+# city = next most restrictive = 765, 667, 813
 d %<>% select(-most.recent.contributor.zipcode)
 d %<>% mutate(most.recent.contributor.city = tolower(WORK_CITY))
-d5 <- inner_join(d, dime) 
+d5 <- inner_join(d, dime) %>% mutate(match_method = "first-initial-city")
 write.csv(d5 %>% select(match, LAST_NAME, FIRST_NAME, MIDDLE_NAME, most.recent.contributor.name, WORK_CITY, most.recent.contributor.city, WORK_ADDRESS_1,WORK_ADDRESS_2,most.recent.contributor.address, most.recent.contributor.occupation, most.recent.contributor.employer, bonica.cid, id), 
           "data/DIME-DSG-first-initial-city-matches.csv")
 d %<>% filter(!id %in% d5$id)
 
-# state = next most restrictive match = 4130, 2182
+# state = next most restrictive match = 4130, 2182, 2690
 d %<>% select(-most.recent.contributor.city)
 d %<>% mutate(most.recent.contributor.state = STATE_PROVINCE)
-d6 <- inner_join(d, dime) 
+d6 <- inner_join(d, dime) %>% mutate(match_method = "fullname-state")
 write.csv(d6 %>% select(match, LAST_NAME, FIRST_NAME, MIDDLE_NAME, most.recent.contributor.name, WORK_CITY, most.recent.contributor.city, WORK_ADDRESS_1,WORK_ADDRESS_2,most.recent.contributor.address, most.recent.contributor.occupation, most.recent.contributor.employer, bonica.cid, id), 
           "data/DIME-DSG-fullname-state-matches.csv")
 d %<>% filter(!id %in% d6$id)
@@ -112,3 +120,10 @@ n_distinct(df)
 # top professions
 df %>% group_by(most.recent.contributor.occupation) %>% 
   summarise(n = n()) %>% arrange(-n)
+
+
+
+
+
+
+
--- readDIME.R
+++ readDIME.R
@@ -13,8 +13,12 @@ source(here("setup.R"))
 # CGS #
 #######
 d <- readxl::read_xlsx(here("data/CSG Directory.xlsx")) %>% 
+  as_tibble() %>% 
   filter(!is.na(LAST_NAME)) %>% 
   mutate(last_name = tolower(LAST_NAME) ) 
+d
+# inspect 
+head(d$last_name)
 
 # GET DIME DATA (careful, this is >3GB)
 # download.file("https://dataverse.harvard.edu/api/access/datafile/2865300?gbrecs=true", 
@@ -26,25 +30,52 @@ d <- readxl::read_xlsx(here("data/CSG Directory.xlsx")) %>%
 load("data/dime_contributors_1979_2018.rdata")
 dime <- contribs %>% as_tibble()
 
+dime %<>% mutate_if(is.factor, as.character)
+
+names(dime)
+
 # TRIM DOWN
 dime %<>% filter(contributor.type == "I")
-names(dime)
-head(dime$last_name)
+
+# inspect
 dime %>% count(is.na(amount_2018)) # note a lot of 0s here may be NAs because SQL data on PAC data shows incomplete data for 2018 cycle
+dime %>% count(amount_2012 == 0)
+dime %>% count(amount_2016 == 0)
+dime %>% count(amount_2018 == 0)
+dime %>% count(bonica.cid, sort = T)
+
+# make last name string 
 dime %<>% mutate(last_name = str_extract(dime$most.recent.contributor.name, "[a-z]*") )
+
+# inspect 
+head(dime$last_name)
 
 # Subset to last names in d (from data/CSG Directory.xlsx)
 dime %<>% filter(last_name %in% unique(d$last_name)) # LAST_NAME?
 
+# inspect
+dime
+
 # Clean up DIME names
 dime %<>% mutate(name = str_replace(most.recent.contributor.name, " dr | jr |[0-9]", " ") )
-dime %<>% mutate(name = str_replace(most.recent.contributor.name, "  ", " ") )
+dime %<>% mutate(name = str_squish(name))
+
+# first name is any string of more than 2 letters after the comma in the cleaned up dime name
 dime %<>% mutate(first_name = str_extract(name, ", [a-z][a-z]*") )
 dime %<>% mutate(first_name = str_replace(first_name, ", ", ""))
-dime %<>% mutate(first_name2 = str_extract(dime$most.recent.contributor.name, ", [a-z]{2}*") )
+
+# alternativly, use the original dime name 
+dime %<>% mutate(first_name2 = str_extract(dime$most.recent.contributor.name, ", [a-z][a-z]*") )
+dime %<>% mutate(first_name2 = str_replace(first_name2, ", ", ""))
+
+# middle initial is any single character at the end of the name string
 dime %<>% mutate(middle_initial = str_extract(name, " [a-z]$") )
 dime %<>% mutate(middle_initial = str_replace(middle_initial, " ", "") )
+
+# first initial from first name
 dime$first_initial <- str_sub(dime$first_name, 1,1)
+
+dime
 
 # SAVE
 save(dime, file = here("data/dime_CGS_matches.Rdata"))
